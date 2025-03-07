4c51558d3c87431ce50ad933f6c927b8f48d567f
anas-waleed-tahir <anastahir707070@gmail.com>
2025-03-07 15:11:37 +0500

    Update API URLs to include review endpoints for WatchList; remove redundant stream review paths

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -4,9 +4,9 @@ from .views import ReviewCreateView, ReviewDetailView, ReviewListView, WatchList
 urlpatterns = [
     path("list/", WatchListView.as_view(), name="WatchList-list"),
     path("list/<int:pk>/", WatchListDetailView.as_view(), name="WatchList-detail"),
+    path("list/<int:pk>/review/", ReviewListView.as_view(), name="Review-list"),
+    path("list/review/<int:pk>/", ReviewDetailView.as_view(), name="Review-detail"),
+    path("list/<int:pk>/review-create/", ReviewCreateView.as_view(), name="Review-create"),
     path("stream/", StreamPlatformListView.as_view(), name="Stream-list"),
     path("stream/<int:pk>/", StreamPlatformDetailView.as_view(), name="Stream-detail"),
-    path("stream/<int:pk>/review/", ReviewListView.as_view(), name="Review-list"),
-    path("stream/review/<int:pk>/", ReviewDetailView.as_view(), name="Review-detail"),
-    path("stream/<int:pk>/review-create/", ReviewCreateView.as_view(), name="Review-create"),
 ]
