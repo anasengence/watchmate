55e0a9e30b08f52db3a14c1804ea1bb03351d017
anas-waleed-tahir <anastahir707070@gmail.com>
2025-03-11 13:15:01 +0500

    Add logout functionality and update user API URLs

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
-from user_app.api.views import RegisterView
+from user_app.api.views import LogoutView, RegisterView
 
 urlpatterns = [
     path("login/", obtain_auth_token, name="Login"),
     path("register/", RegisterView.as_view(), name="Register"),
+    path("logout/", LogoutView.as_view(), name="Logout"),
 ]
--- user_app/api/views.py
+++ user_app/api/views.py
@@ -2,6 +2,8 @@ from rest_framework import generics
 from django.contrib.auth.models import User
 from .serializers import RegistrationSerializer
 from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class RegisterView(generics.CreateAPIView):
@@ -18,3 +20,11 @@ class RegisterView(generics.CreateAPIView):
         token = Token.objects.get(user=response.data["id"])
         response.data["token"] = token.key
         return response
+
+
+class LogoutView(generics.DestroyAPIView):
+    queryset = User.objects.all()
+
+    def delete(self, request):
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
