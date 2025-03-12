68dfe8aa4f439baa91b312019302e2abe8670258
anas-waleed-tahir <anastahir707070@gmail.com>
2025-03-12 12:53:49 +0500

    Add JWT authentication support; update API URLs for token management

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from user_app.api.views import LogoutView, RegisterView
 
 urlpatterns = [
     path("login/", obtain_auth_token, name="Login"),
     path("register/", RegisterView.as_view(), name="Register"),
     path("logout/", LogoutView.as_view(), name="Logout"),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
