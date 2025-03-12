406746838f02f0986f7016e0bfdbb7abd252a1d9
anas-waleed-tahir <anastahir707070@gmail.com>
2025-03-12 14:00:49 +0500

    Implement JWT token generation in user registration; remove legacy token creation

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -3,7 +3,11 @@ from django.contrib.auth.models import User
 from .serializers import RegistrationSerializer
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import status
+
+
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class RegisterView(generics.CreateAPIView):
@@ -13,12 +17,17 @@ class RegisterView(generics.CreateAPIView):
 
     def perform_create(self, serializer):
         user = serializer.save()
-        Token.objects.create(user=user)
+        # Token.objects.create(user=user)
 
     def post(self, request):
         response = self.create(request)
-        token = Token.objects.get(user=response.data["id"])
-        response.data["token"] = token.key
+        # token = Token.objects.get(user=response.data["id"])
+        # response.data["token"] = token.key
+        refresh = RefreshToken.for_user(response.data["id"])
+        response.data["token"] = {
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }
         return response
 
 
